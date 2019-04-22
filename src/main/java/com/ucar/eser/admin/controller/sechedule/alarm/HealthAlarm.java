3f4ce71edcaad91b38fef799e5d11ea005e47f15
yin.he <1140466863@qq.com>
2019-04-22 16:45:55 +0800

    解决bug

--- src/main/java/com/ucar/eser/admin/controller/sechedule/alarm/HealthAlarm.java
+++ src/main/java/com/ucar/eser/admin/controller/sechedule/alarm/HealthAlarm.java
@@ -23,11 +23,12 @@ import java.util.Map;
  */
 public class HealthAlarm extends KeyCallable<Boolean> {
 
-	private AlarmRuleService alarmRuleService = (AlarmRuleService) SpringInit.getApplicationContext().getBean("alarmRuleServiceImpl");
+	private AlarmRuleService alarmRuleService = (AlarmRuleService) SpringInit.getApplicationContext()
+			.getBean("alarmRuleServiceImpl");
 
 	private String ALARM_TITLE;
 
-	private static Map<String,Object> alarmContinueMap;
+	private static Map<String, Object> alarmContinueMap;
 
 	private QueryResult oldData;
 
@@ -37,20 +38,20 @@ public class HealthAlarm extends KeyCallable<Boolean> {
 
 	private NodeCommonStatInfo nodeCommonStatInfo;
 
-
 	private Integer redAlarmInterval;
 
 	private Integer yellowAlarmInterval;
 
-	public HealthAlarm(String key, String clusterName, String newResult, QueryResult oldResult,NodeCommonStatInfo nodeCommonStatInfo) {
+	public HealthAlarm(String key, String clusterName, String newResult, QueryResult oldResult,
+			NodeCommonStatInfo nodeCommonStatInfo) {
 		super(key);
 		this.newData = newResult;
 		this.oldData = oldResult;
 		this.clusterName = clusterName;
 		this.nodeCommonStatInfo = nodeCommonStatInfo;
 		ALARM_TITLE = "es集群健康报警";
-		redAlarmInterval = 1*60*1000;
-		yellowAlarmInterval = 5*60*1000;
+		redAlarmInterval = 1 * 60 * 1000;
+		yellowAlarmInterval = 5 * 60 * 1000;
 	}
 
 	@Override
@@ -59,86 +60,85 @@ public class HealthAlarm extends KeyCallable<Boolean> {
 			checkClusterHealth();
 			return true;
 		} catch (Exception e) {
-			LOGGER.error(clusterName+",健康检查异常", e);
+			LOGGER.error(clusterName + ",健康检查异常", e);
 		}
 		return false;
 	}
-	
-	
-	 /**
-     * 
-     * Description: 检查集群健康度
-     */
-    private void checkClusterHealth() throws Exception {
-    	//从服务端获取集群健康信息
-    	if(newData == null) {
-    		return;
-    	}
-		AlarmRule alarmRule = alarmRuleService.getAlarmRuleByClusterName(clusterName);
-		if(alarmRule == null) {
+
+	/**
+	* 
+	* Description: 检查集群健康度
+	*/
+	private void checkClusterHealth() throws Exception {
+		//从服务端获取集群健康信息
+		if (newData == null) {
 			return;
 		}
-    	//解析服务端返回的结果
-    	ClusterHealthVo healthVo = JSONObject.parseObject(newData, ClusterHealthVo.class);
+		AlarmRule alarmRule = alarmRuleService.getAlarmRuleByClusterName(clusterName);
+		if (alarmRule == null) {
+			return;
+		}
+		//解析服务端返回的结果
+		ClusterHealthVo healthVo = JSONObject.parseObject(newData, ClusterHealthVo.class);
 		String title = String.format("【ESER】%s,【集群:%s】", ConfigUtil.I.envTypeName(), clusterName);
 		String healthState = healthVo.getStatus();
 		//解析influxdb数据库数据
-		String oldHealthState =null;
-		if(InflusDbUtil.FLAG){
-			Map<String,Object> statMap = InflusDbUtil.query(oldData,0,0,1);
-			if(statMap != null){
+		String oldHealthState = null;
+		if (InflusDbUtil.FLAG) {
+			Map<String, Object> statMap = InflusDbUtil.query(oldData, 0, 0, 1);
+			if (statMap != null) {
 				oldHealthState = (String) statMap.get("result");
 			}
-		}else{
-			oldHealthState= nodeCommonStatInfo.getClusterStatus();
+		} else {
+			oldHealthState = nodeCommonStatInfo.getClusterStatus();
 		}
-		if("green".equals(healthState)) {
-			if(oldHealthState != null && !oldHealthState.equals("green")) {
-				String content = title+"恢复正常";
-				MailClient.sendMessage(null,content,ALARM_TITLE);
+		if ("green".equals(healthState)) {
+			if (oldHealthState != null && !oldHealthState.equals("green")) {
+				String content = title + "恢复正常";
+				MailClient.sendMessage(null, content, ALARM_TITLE);
 			}
-			if(alarmContinueMap != null){
-				alarmContinueMap.put("yellowcount"+clusterName,null);
-				alarmContinueMap.put("yellowcheckTime"+clusterName,null);
-				alarmContinueMap.put("redcount"+clusterName,null);
-				alarmContinueMap.put("redcheckTime"+clusterName,null);
+			if (alarmContinueMap != null) {
+				alarmContinueMap.put("yellowcount" + clusterName, null);
+				alarmContinueMap.put("yellowcheckTime" + clusterName, null);
+				alarmContinueMap.put("redcount" + clusterName, null);
+				alarmContinueMap.put("redcheckTime" + clusterName, null);
 			}
 			return;
 		}
 		Integer count = 1;
-		if( alarmContinueMap != null && alarmContinueMap.get(healthState+"count"+clusterName) != null){
-			count += (Integer) alarmContinueMap.get(healthState+"count"+clusterName);
-		}else{
+		if (alarmContinueMap != null && alarmContinueMap.get(healthState + "count" + clusterName) != null) {
+			count += (Integer) alarmContinueMap.get(healthState + "count" + clusterName);
+		} else {
 			alarmContinueMap = new HashMap<String, Object>();
 		}
-		StringBuilder alarmMsg = new StringBuilder(title+"当前状态为："+healthVo.getStatus() +"，此为第"+count+"次报警");
+		StringBuilder alarmMsg = new StringBuilder(title + "当前状态为：" + healthVo.getStatus() + "，此为第" + count + "次报警");
 		//若前后两次状态一致，则进行持续时间检测
-		Map<String,Object> timeMap = new HashMap<String, Object>();
-		if(InflusDbUtil.FLAG){
-			timeMap =InflusDbUtil.query(oldData,0,0,2);
-		}else{
+		Map<String, Object> timeMap = new HashMap<String, Object>();
+		if (InflusDbUtil.FLAG) {
+			timeMap = InflusDbUtil.query(oldData, 0, 0, 2);
+		} else {
 			timeMap.put("result", DateUtil.formatYYYYMMddHHMMSS(nodeCommonStatInfo.getCreateTime()));
 		}
-		if(oldHealthState != null && healthState.equals(oldHealthState)) {
+		if (oldHealthState != null && healthState.equals(oldHealthState)) {
 			String oldCheckTimeStr = checkTimeInit(healthState, timeMap);
-			Long oldCheckTime = DateUtil.parse(oldCheckTimeStr,"yyyy-MM-dd HH:mm:ss").getTime();
+			Long oldCheckTime = DateUtil.parse(oldCheckTimeStr, "yyyy-MM-dd HH:mm:ss").getTime();
 			long diff = healthVo.getCheckTime() - oldCheckTime;
 
 			//若为第一次变为异常状态，则直接报警，否则只在持续一定时间再次报警
-			if("yellow".equals(healthState)){
-				if(2*diff/yellowAlarmInterval < count*(count - 1)) {
+			if ("yellow".equals(healthState)) {
+				if (2 * diff / yellowAlarmInterval < count * (count - 1)) {
 					return;
 				}
-			}else if("red".equals(healthState)){
-				if(2*diff/redAlarmInterval < count*(count - 1)) {
+			} else if ("red".equals(healthState)) {
+				if (2 * diff / redAlarmInterval < count * (count - 1)) {
 					return;
 				}
 			}
-			String str = ",此状态已持续"+diff+"毫秒";
+			String str = ",此状态已持续" + diff + "毫秒";
 			alarmMsg.append(str);
-			alarmContinueMap.put(healthState+"count"+clusterName,count);
-		}else{
-			alarmContinueMap.put(healthState+"count"+clusterName,count);
+			alarmContinueMap.put(healthState + "count" + clusterName, count);
+		} else {
+			alarmContinueMap.put(healthState + "count" + clusterName, count);
 			checkTimeInit(healthState, timeMap);
 		}
 		alarmMsg.append("<br>");
@@ -149,19 +149,19 @@ public class HealthAlarm extends KeyCallable<Boolean> {
 		alarmMsg.append("，未分配的分片数：");
 		alarmMsg.append(healthVo.getUnassigned_shards());
 
-		MailClient.sendMessage(null,alarmMsg.toString(),ALARM_TITLE,ConfigUtil.I.envType());
+		// 这是一个bug 先这样写死解决
+		MailClient.sendMessage("1140466863@qq.com", alarmMsg.toString(), ALARM_TITLE, ConfigUtil.I.envType());
 	}
 
 	private String checkTimeInit(String healthState, Map<String, Object> timeMap) {
 		String oldCheckTimeStr;
-		if(alarmContinueMap.get(healthState+"checkTime"+clusterName) != null ){
-            oldCheckTimeStr = (String) alarmContinueMap.get(healthState+"checkTime"+clusterName);
-        }else{
-            oldCheckTimeStr= (String) timeMap.get("result");
-            alarmContinueMap.put(healthState+"checkTime"+clusterName,oldCheckTimeStr);
-        }
+		if (alarmContinueMap.get(healthState + "checkTime" + clusterName) != null) {
+			oldCheckTimeStr = (String) alarmContinueMap.get(healthState + "checkTime" + clusterName);
+		} else {
+			oldCheckTimeStr = (String) timeMap.get("result");
+			alarmContinueMap.put(healthState + "checkTime" + clusterName, oldCheckTimeStr);
+		}
 		return oldCheckTimeStr;
 	}
 
 }
-
--- src/main/java/com/ucar/eser/core/util/InflusDbUtil.java
+++ src/main/java/com/ucar/eser/core/util/InflusDbUtil.java
@@ -18,67 +18,68 @@ import java.util.concurrent.TimeUnit;
  */
 public class InflusDbUtil {
 
-    protected static Logger LOGGER = LoggerFactory.getLogger(InflusDbUtil.class);
+	protected static Logger LOGGER = LoggerFactory.getLogger(InflusDbUtil.class);
 
-    private static InfluxDB influxDB;
+	private static InfluxDB influxDB;
 
-    public static String HOST_PORT;
+	public static String HOST_PORT;
 
-    public static String USER_NAME;
+	public static String USER_NAME;
 
-    public static String PWD;
+	public static String PWD;
 
-    public static String DATABASE;
+	public static String DATABASE;
 
-    public static Boolean FLAG;
+	public static Boolean FLAG;
 
-    static {
-        PropertiesConfiguration propertiesConfiguration = null;
-        try {
-            propertiesConfiguration = new PropertiesConfiguration("influxdb.properties");
-        } catch (ConfigurationException e) {
-            e.printStackTrace();
-        }
-        if(propertiesConfiguration != null){
-            HOST_PORT = propertiesConfiguration.getString("HOST_PORT");
-            USER_NAME = propertiesConfiguration.getString("USER_NAME");
-            PWD = propertiesConfiguration.getString("PWD");
-            DATABASE = propertiesConfiguration.getString("DATABASE");
-            FLAG = Boolean.valueOf(propertiesConfiguration.getString("FLAG"));
-            try {
-                influxDB = InfluxDBFactory.connect(HOST_PORT, USER_NAME, PWD);
-                influxDB.enableBatch(2000, 100, TimeUnit.MILLISECONDS);
-            }catch (Exception e){
-                LOGGER.error("influxdb connect error:",e);
-            }
-        }
-    }
+	static {
+		PropertiesConfiguration propertiesConfiguration = null;
+		try {
+			propertiesConfiguration = new PropertiesConfiguration("influxdb.properties");
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
+		if (propertiesConfiguration != null) {
+			HOST_PORT = propertiesConfiguration.getString("HOST_PORT");
+			USER_NAME = propertiesConfiguration.getString("USER_NAME");
+			PWD = propertiesConfiguration.getString("PWD");
+			DATABASE = propertiesConfiguration.getString("DATABASE");
+			FLAG = Boolean.valueOf(propertiesConfiguration.getString("FLAG"));
+			try {
+				if (FLAG) {
+                    influxDB = InfluxDBFactory.connect(HOST_PORT, USER_NAME, PWD);
+                    influxDB.enableBatch(2000, 100, TimeUnit.MILLISECONDS);
+				}
+			} catch (Exception e) {
+				LOGGER.error("influxdb connect error:", e);
+			}
+		}
+	}
 
+	public static InfluxDB getConnection() {
+		if (influxDB == null) {
+			synchronized (InflusDbUtil.class) {
+				influxDB = InfluxDBFactory.connect(HOST_PORT, USER_NAME, PWD);
+				influxDB.enableBatch(2000, 100, TimeUnit.MILLISECONDS);
+			}
+		}
+		return influxDB;
+	}
 
-    public static InfluxDB getConnection(){
-        if(influxDB == null ){
-            synchronized (InflusDbUtil.class){
-                influxDB = InfluxDBFactory.connect(HOST_PORT, USER_NAME, PWD);
-                influxDB.enableBatch(2000, 100, TimeUnit.MILLISECONDS);
-            }
-        }
-        return influxDB;
-    }
+	public static QueryResult query(String q) {
+		return getConnection().query(new Query(q, DATABASE));
+	}
 
-
-    public static QueryResult query(String q){
-        return getConnection().query(new Query(q,DATABASE));
-    }
-
-
-    public static Map<String,Object> query(QueryResult queryResult,Integer seriesIndex,Integer valRow,Integer indexVal){
-        Map<String,Object> map =new HashMap<String, Object>();
-        String result = null;
-       if(valRow != null && indexVal != null && queryResult != null){
-           result =queryResult.getResults().get(0).getSeries().get(seriesIndex).getValues().get(valRow).get(indexVal).toString();
-           map.put("result",result);
-       }
-        return map;
-    }
+	public static Map<String, Object> query(QueryResult queryResult, Integer seriesIndex, Integer valRow,
+			Integer indexVal) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		String result = null;
+		if (valRow != null && indexVal != null && queryResult != null) {
+			result = queryResult.getResults().get(0).getSeries().get(seriesIndex).getValues().get(valRow).get(indexVal)
+					.toString();
+			map.put("result", result);
+		}
+		return map;
+	}
 
 }
